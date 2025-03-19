43ffdb79147661e2f117c95ccc431c0d1819919c
yamone178 <yamonemyatmoe1592@gmail.com>
2025-03-19 15:03:38 +0630

    fix zodiacseeder

--- database/seeders/ZodiacSeeder.php
+++ database/seeders/ZodiacSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class ZodiacSeeder extends Seeder
 {
@@ -13,7 +14,7 @@ class ZodiacSeeder extends Seeder
     public function run(): void
     {
         // Path to the CSV file stored in the storage/app directory
-        $file = storage_path('app/zodiacs.csv');
+        $file = Storage::get('zodiacs.csv');
         // Open and read the CSV file
         $csv = array_map('str_getcsv', file($file));
         // Remove the header row (if present)
@@ -21,10 +22,14 @@ class ZodiacSeeder extends Seeder
 
         // Loop through each row in the CSV file
         foreach ($csv as $row) {
+            if (count($row) < 4) {
+                throw new \Exception("Invalid CSV row: " . json_encode($row));
+            }
+        
             DB::table('zodiacs')->insert([
                 'id' => $row[0],
                 'name' => $row[1],
-                'from_date' => $row[2],  
+                'from_date' => $row[2],
                 'to_date' => $row[3],
             ]);
         }
